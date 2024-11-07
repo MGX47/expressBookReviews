c7a9dee51f656dfcceb84510fc926bcf730d032a
= <Michael.g.47@gmail.com>
2024-11-07 12:40:18 -0500

    general routes impl

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,38 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const allBooks = books;
+    return res.status(200).send(JSON.stringify({allBooks}, null, 4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const bookByIsbn = books[isbn];
+    return res.status(200).send(JSON.stringify({bookByIsbn}, null, 4))
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const booksByAuthor = Object.values(books).filter(book => book.author === author);
+    return res.status(200).send(JSON.stringify({booksByAuthor}, null, 4))
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.title;
+    const booksByTitle = Object.values(books).filter(book => book.title === title);
+    return res.status(200).send(JSON.stringify({booksByTitle}, null, 4))
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const bookByIsbn = books[isbn] || {};
+    const bookReviews = bookByIsbn.reviews;
+    return res.status(200).send(JSON.stringify({bookReviews}, null, 4))
 });
 
 module.exports.general = public_users;
